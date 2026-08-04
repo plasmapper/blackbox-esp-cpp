3c8f3e2331d2d2a65166658789d2736e4b5c2d38
plasmapper <99073058+plasmapper@users.noreply.github.com>
2026-08-04 16:19:13 +0200

    Changed lock timeout handling

--- component/pl_blackbox_hardware_interface_configuration.cpp
+++ component/pl_blackbox_hardware_interface_configuration.cpp
@@ -21,18 +21,15 @@ BlackBoxHardwareInterfaceConfiguration::BlackBoxHardwareInterfaceConfiguration(s
 
 esp_err_t BlackBoxHardwareInterfaceConfiguration::Lock(TickType_t timeout) {
   esp_err_t error = mutex.Lock(timeout);
-  if (error == ESP_OK)
-    return ESP_OK;
-  if (error == ESP_ERR_TIMEOUT && timeout == 0)
-    return ESP_ERR_TIMEOUT;
-  ESP_RETURN_ON_ERROR(error, TAG, "mutex lock failed");
-  return ESP_OK;
+  if (error != ESP_OK && (error != ESP_ERR_TIMEOUT || timeout != 0))
+    ESP_LOGE(TAG, "mutex lock failed");
+  return error;
 }
 
 //==============================================================================
 
 esp_err_t BlackBoxHardwareInterfaceConfiguration::Unlock() {
-  ESP_RETURN_ON_ERROR(mutex.Unlock(), CONFIG_PARAM_TAG, "mutex unlock failed");
+  ESP_RETURN_ON_ERROR(mutex.Unlock(), TAG, "mutex unlock failed");
   return ESP_OK;
 }
 
